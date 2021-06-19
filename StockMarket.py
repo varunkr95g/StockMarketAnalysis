3e3a5bddd6907569b0df6181e83c4924e41c86fb
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-06-19 22:57:15 +0530

    Import the data from Yahoo Finance

--- StockMarket.py
+++ StockMarket.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 from nsedata import Nse
 
-
+#Import the data from Yahoo Finance
 df=pd.read_html("https://in.finance.yahoo.com/quote/SANGHIIND.NS?p=SANGHIIND.NS")[0]
 data=yf.download("SANGHIIND.NS","2021-01-01")
 data.to_csv('data_Sanghi.csv')

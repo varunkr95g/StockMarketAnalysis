4c8cab5a2fc614e247b418b786861e2bd0ea47f5
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-06-21 20:06:11 +0530

    Update BearishEngulfingPattern.py

--- BearishEngulfingPattern.py
+++ BearishEngulfingPattern.py
@@ -5,24 +5,25 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 df=pd.read_csv("data_Sanghi.csv",usecols=['Date','Open','High','Low','Close'])
 
-#Bullsih Engulfing pattern is a multiple candlestick pattern. It's formed when on Day 1 there's a downtrend
-# and on Day 2 there's an upward trend and the Close of Day 2 > Open of Day 1. The body of Day 2 should also
+#Bearish Engulfing pattern is a multiple candlestick pattern. It's formed when on Day 1 there's a uptrend
+# and on Day 2 there's an downward trend and the Close of Day 2 < Open of Day 1. The body of Day 2 should also
 # be greater than body of Day 1. Also, I have put the criteria of at least a percent difference between
 # Open and Close on Day 1 to prevent Dojis from being considered.
 
-df['BearishEngulfingPattern']=np.where((df.Open.shift(1)<df.Close.shift(1)) & (df["Open"]>df.Close.shift(1))
-                                       & ((df["Open"]-df["Close"])>(df.Close.shift(1) - df.Open.shift(1)))
+df['BearishEngulfingPattern']=np.where((df.Open.shift(1)<df.Close.shift(1)) & (df["Close"]<df.Open.shift(1))
+                                       & (abs(df["Open"]-df["Close"])>abs(df.Close.shift(1) - df.Open.shift(1)))
                                        & ((df.Close.shift(1) - df.Open.shift(1))/df.Open.shift(1)>0.01) & (df["Date"]>'04/01/21'),1,0)
 
 rslt_df = df[df['BearishEngulfingPattern']==1]
 
 print(rslt_df[["Date","Open","Close","BearishEngulfingPattern"]])
 
+#Check for prior Bullish pattern
 prior_bullish_pattern_exists=[]
 
 for i in range(len(rslt_df)):
     closed_values = []
-    for j in range(1,4):
+    for j in range(1,6):
         closed_values.append(df.loc[df['BearishEngulfingPattern'].shift(-j) == 1, 'Open'].iloc[i])
 
     # print(closed_values)

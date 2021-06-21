c25b69a071e0dcd3bb3bd424df47ee29e9e22b66
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-06-21 16:00:09 +0530

    Update BullishEngulfingPattern.py

--- BullishEngulfingPattern.py
+++ BullishEngulfingPattern.py
@@ -5,6 +5,11 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 df=pd.read_csv("data_Sanghi.csv",usecols=['Date','Open','High','Low','Close'])
 
+#Bullsih Engulfing pattern is a multiple candlestick pattern. It's formed when on Day 1 there's a downtrend
+# and on Day 2 there's an upward trend and the Close of Day 2 > Open of Day 1. The body of Day 2 should also
+# be greater than body of Day 1. Also, I have put the criteria of at least a percent difference between
+# Open and Close on Day 1 to prevent Dojis from being considered.
+
 df['BullsihEngulfingPattern']=np.where((df.Open.shift(1)>df.Close.shift(1)) & (df["Close"]>df.Open.shift(1))
                                        & ((df["Close"]-df["Open"])>(df.Open.shift(1) - df.Close.shift(1)))
                                        & ((df.Open.shift(1) - df.Close.shift(1))/df.Open.shift(1)>0.01),1,0)
